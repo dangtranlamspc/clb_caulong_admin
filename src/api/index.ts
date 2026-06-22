11a4b6aed69c03495089e39ba36d341fc4bc2465
ngoiv03-alt <ngoi.v03@gmail.com>
2026-06-22 15:34:11 +0700

    fix url api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:3001/api/v1';
+const API_BASE = import.meta.env.VITE_API_URL
 
 export const api = axios.create({
   baseURL: API_BASE,
